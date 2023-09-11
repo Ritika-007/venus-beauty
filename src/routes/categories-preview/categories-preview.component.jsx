b5e4f0d62c53a20b122105a2fb727696dd3607ed
Ritika-007 <91926843+Ritika-007@users.noreply.github.com>
2023-09-11 23:26:02 +0530

    Update categories-preview.component.jsx

--- src/routes/categories-preview/categories-preview.component.jsx
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -1,10 +1,11 @@
 import { useContext, Fragment } from 'react';
 
 import { CategoriesContext } from '../../context/categories.context';
-// This line imports the CategoriesContext from a relative path. This context likely provides information about categories and their associated products.
+//  provides information about categories and their associated products.
 import CategoryPreview from '../../components/category-preview/category-preview.component';
 
-// CategoriesPreview component uses the CategoriesContext to access the categoriesMap and then maps over the keys of this map to render CategoryPreview components for each category. 
+// CategoriesPreview component uses the CategoriesContext to access the categoriesMap and then maps over the keys of 
+// this map to render CategoryPreview components for each category. 
 // This component is responsible for displaying previews of various categories and their associated products on a page.
 
 const CategoriesPreview = () => {

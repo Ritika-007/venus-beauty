c11108b866db03283cc7abe78a72ed284eca497d
Ritika-007 <91926843+Ritika-007@users.noreply.github.com>
2023-09-11 23:27:33 +0530

    Update home.component.jsx

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,6 +1,7 @@
 import Directory from "../../components/directory/directory.component";
 
-// Home component serves as a container for rendering the Directory component, which is responsible for displaying the directory or menu of categories or sections on the homepage of your application.
+// Home component serves as a container for rendering the Directory component, which is responsible for displaying the directory or
+// menu of categories or sections on the homepage the application.
  
 const Home = () => {
   return (

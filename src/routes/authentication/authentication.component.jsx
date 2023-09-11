a85c2a5e31cbd7ad4b2f758066f005a28a063152
Ritika-007 <91926843+Ritika-007@users.noreply.github.com>
2023-09-11 22:05:32 +0530

    Update authentication.component.jsx

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -2,6 +2,9 @@ import SignUpForm from "../../components/sign-up-form/sign-up-form.component"
 import SignInForm from "../../components/sign-in-form/sign-in-form.component"
 import './authentication.styles.scss'
 
+// Authentication component imports and renders two other components (SignInForm and SignUpForm) to create a user authentication page. 
+// The imported components are responsible for handling user login and registration.
+
 const Authentication = () => {
   return (
     <div className="authentication-container">

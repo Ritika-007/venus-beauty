cb336f14885d585de51814e21ea5cd7fe89890fb
Ritika-007 <91926843+Ritika-007@users.noreply.github.com>
2023-09-11 22:04:29 +0530

    Update button.component.jsx

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -1,3 +1,10 @@
+/*
+So, this Button component is designed to create a button element with dynamic styling based 
+on its buttonType prop. It applies CSS classes to the button based on the type of button, 
+and you can pass additional props to the underlying <button> element using the otherProps 
+spread. The component is also designed to render any content you provide between the <Button>
+tags as the button's content.
+*/
 import "./button.styles.scss";
 
 export const BUTTON_TYPE_CLASSES = {
@@ -6,6 +13,7 @@ export const BUTTON_TYPE_CLASSES = {
 };
 
 const Button = ({ children, buttonType, ...otherProps }) => {
+  
   return (
     <button
       className={`button-container ${BUTTON_TYPE_CLASSES[buttonType]}`}

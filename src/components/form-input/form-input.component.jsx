31d2afc425449d549f9cb1fed3d3dc8ea2f2bb94
Ritika-007 <91926843+Ritika-007@users.noreply.github.com>
2023-09-11 23:34:11 +0530

    Update form-input.component.jsx

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -1,6 +1,7 @@
 import './form-input.styles.scss'
 
-// FormInput component is used to render an input field in a form. It can be configured with a label, and when the user interacts with the input, the label can shrink or move to provide a better user experience. 
+// FormInput component is used to render an input field in a form. It can be configured with a label, 
+// and when the user interacts with the input, the label can shrink or move to provide a better user experience. 
 // The component accepts additional props that can be passed down to the underlying <input> element.
 
 const FormInput = ({ label, ...otherProps }) => {

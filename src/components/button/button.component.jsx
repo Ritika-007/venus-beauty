917112ad0b23afdcd6b8f4c46db547190a7398f4
Ritika-007 <91926843+Ritika-007@users.noreply.github.com>
2023-09-11 23:31:00 +0530

    Update button.component.jsx

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -1,12 +1,5 @@
-/*
-So, this Button component is designed to create a button element with dynamic styling based 
-on its buttonType prop. It applies CSS classes to the button based on the type of button, 
-and you can pass additional props to the underlying <button> element using the otherProps 
-spread. The component is also designed to render any content you provide between the <Button>
-tags as the button's content.
-*/
 import "./button.styles.scss";
-
+// button component and has 2 types
 export const BUTTON_TYPE_CLASSES = {
   google: "google-sign-in",
   inverted: "inverted",

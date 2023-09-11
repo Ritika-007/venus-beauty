2bc539276501147b5b5dfcfa8e07a699789e3f2e
Ritika-007 <91926843+Ritika-007@users.noreply.github.com>
2023-09-11 22:04:33 +0530

    Update cart-icon.component.jsx

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -6,6 +6,10 @@ import { CartContext } from '../../context/cart.context';
 
 import './cart-icon.styles.scss';
 
+// CartIcon component is designed to display a shopping cart icon along with a count of items in the cart. 
+// When clicked, it toggles the cart's visibility by modifying the isCartOpen state from the context. 
+// The cart data and state are managed through the CartContext.
+
 const CartIcon = () => {
   const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
 

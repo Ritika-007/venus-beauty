4be9a81696c069b0865b3f184a2e5c97269eaed3
Ritika-007 <91926843+Ritika-007@users.noreply.github.com>
2023-09-11 22:04:54 +0530

    Update directory-item.component.jsx

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,6 +1,10 @@
 import { useNavigate } from 'react-router-dom';
 import './directory-item.styles.scss';
 
+// DirectoryItem component is designed to represent a category item in a directory. 
+// It displays the category's background image, title, and provides an onClick event handler to navigate to a specific route associated with the category. 
+// The useNavigate hook is used to handle the navigation logic.
+
 const DirectoryItem = ({ category }) => {
   const { imageUrl, title, route } = category;
   const navigate = useNavigate()
